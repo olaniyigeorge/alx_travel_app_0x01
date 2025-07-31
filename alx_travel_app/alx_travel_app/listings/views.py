73415ea5f921b1748d398597c355cf6e5438179c
olaniyigeorge <olaniyigeorge77@gmail.com>
2025-07-31 14:19:03 +0100

    api_dev

---
+++ alx_travel_app/alx_travel_app/listings/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET'])
+def index(request):
+    """
+    Basic API endpoint for listings
+    """
+    return Response({
+        'message': 'Welcome to ALX Travel App API',
+        'version': 'v1',
+        'app': 'listings'
+    })
+
+
+
+class ListingViewSet(viewsets.ModelViewSet):
+    queryset = Listing.objects.all()
+    serializer_class = ListingSerializer
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
